348b32f930b2ba93c49a2d66052a3196bf71b71d
Demigodofa <granitemfgllc@gmail.com>
2026-02-10 12:32:29 -0500

    Force light theme for consistent text color

--- app/src/main/java/com/kevin/flangejointassembly/MainActivity.kt
+++ app/src/main/java/com/kevin/flangejointassembly/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FlangeJointAssemblyHelperTheme {
+            FlangeJointAssemblyHelperTheme(darkTheme = false) {
                 FlangeApp()
             }
         }
